4b8412000d00cd16975b070f9dcf6bfc32d8299f
Anubhav Pandey <73362441+anubhav-pandey1@users.noreply.github.com>
2021-06-25 01:54:51 +0530

    Update familytree.cpp with comments
    Similar to binary search, we can come up with a predicate that can take Nth generation as an array and decide where to go based on K

--- familytree.cpp
+++ familytree.cpp
@@ -3,24 +3,26 @@ using namespace std;
 
 #define ll long long int
 
-string familyTree(ll n, ll K) {
-    if (n == 1) {
+string familyTree(ll n, ll K) {                                // Bottom-up recursion approach
+    if (n == 1) {                                              // ft(n,K) -> ft(n-1, K/2) --> ft(n-2, K/4)
         return "Male\n";
     }
 
-    if (familyTree(n - 1, ceil((float)K / 2)) == "Male\n") {
+    if (familyTree(n - 1, ceil((float)K / 2)) == "Male\n") {   // If parent is male
         if (K % 2)
             return "Male\n";
         else
             return "Female\n";
     }
-    else {
+    else {                                                     // If parent is female
         if (K % 2)
             return "Female\n";
         else
             return "Male\n";
     }
 }
+
+// Can come up with a binary search or top-down recursion approach
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -44,3 +46,37 @@ int main() {
 
     return 0;
 }
+
+// int main() {
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+//     int t, count;
+//     long long int k, num;
+//     scanf("%d", &t);
+//     while (t--)
+//     {
+//         count = 0;
+//         scanf("%lld %lld", &k, &num);
+//         if (num == 1)
+//             printf("Male\n");
+//         else if (num == 2)
+//             printf("Female\n");
+//         else {
+//             num = num - 1;
+//             while (num > 1)
+//             {
+//                 if (num % 2 != 0)
+//                     count += 1;
+//                 num = num / 2;
+//             }
+//             if (count % 2 == 0)
+//                 printf("Female\n");
+//             else
+//                 printf("Male\n");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
