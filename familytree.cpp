99414dc226a31d12dd7823ca901c25bdea3f7706
Anubhav Pandey <73362441+anubhav-pandey1@users.noreply.github.com>
2021-06-25 02:23:15 +0530

    Update familytree.cpp with top-down approach

--- familytree.cpp
+++ familytree.cpp
@@ -5,24 +5,42 @@ using namespace std;
 
 string familyTree(ll n, ll K) {                                // Bottom-up recursion approach
     if (n == 1) {                                              // ft(n,K) -> ft(n-1, K/2) --> ft(n-2, K/4)
-        return "Male\n";
+        return "Male";
     }
 
-    if (familyTree(n - 1, ceil((float)K / 2)) == "Male\n") {   // If parent is male
-        if (K % 2)
-            return "Male\n";
-        else
-            return "Female\n";
+    if (familyTree(n - 1, ceil((float)K / 2)) == "Male") {     // If parent is male
+        if (K % 2)                                             // Odd child is male, even child female
+            return "Male";                                     // Ceil(K/2) gives K-value for parent
+        else                                                   // Recursion to find gender of parent
+            return "Female";
     }
     else {                                                     // If parent is female
-        if (K % 2)
-            return "Female\n";
+        if (K % 2)                                             // Odd child is female, even child male
+            return "Female";
         else
-            return "Male\n";
+            return "Male";
     }
 }
 
 // Can come up with a binary search or top-down recursion approach
+
+string family_tree(ll n, ll K, string rootGender = "Male") {   // Top-down recursion approach
+    if (n == 1) {                                              // Still ft(n, K) -> ft(n-1, K)
+        return rootGender;
+    }
+
+    ll numChildrenGen = 1LL << (n - 1);                        // Calculating pow(2, n - 1)
+    if (K <= numChildrenGen / 2) {                             // If we go left from the parent for new subtree
+        return family_tree(n - 1, K, rootGender);              // New root gender remains same for new subtree
+    }                                                          // K also remains same since it falls in first half
+    else {                                                     // Else if we go right since K > numChildrenGen/2
+        string newRootGender = "Male";                         // If parent gender = F then subtree parent gender = M
+        if (rootGender = "Male") {                             // If parent gender = M then subtree parent gender = F
+            newRootGender = "Female;"                          // This is because we are going right
+        }                                                      // K needs to be re-index to start from K/2 + 1
+        return family_tree(n - 1, K - numChildrenGen / 2, newRootGender);
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
