dd898db29e1538a94c569c9f027284f78b91cda7
Anubhav Pandey <73362441+anubhav-pandey1@users.noreply.github.com>
2021-06-26 01:24:16 +0530

    Create sortstack.cpp
    Sort a stack using recursion

---
+++ Problems/sortstack.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertStack(stack<int>& s, int n) {
+    if (s.empty()) {
+        s.push(n);
+        return;
+    }
+    if (n >= s.top()) {
+        s.push(n);
+        return;
+    }
+
+    int top = s.top();
+    s.pop();
+    insertStack(s, n);
+    s.push(top);
+}
+
+void sortStack(stack<int>& s) {
+    if (s.empty()) {
+        return;
+    }
+
+    int top = s.top();
+    s.pop();
+    sortStack(s);
+    insertStack(s, top);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int testNum;
+    cin >> testNum;
+    while (testNum--) {
+        int size;
+        cin >> size;
+        stack<int> st;
+        for (int i = 0; i < size; i++) {
+            int ele;
+            cin >> ele;
+            st.push(ele);
+        }
+
+        sortStack(st);
+        for (int i = 0; i < size; i++) {
+            cout << st.top() << " ";
+            st.pop();
+        }
+        cout << endl;
+
+    }
+
+    return 0;
+}
+
+// Test cases:
+
+// Input:
+
+// 1
+// 4
+// 5 1 0 2
+
+// Output:
+
+// 5 2 1 0
